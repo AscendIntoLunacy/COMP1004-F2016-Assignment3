f9f590bdf14facb2cce8e11cf6ab85533b21470c
Ultrabook <Ultrabook@DESKTOP-CI00RK1>
2016-11-02 18:36:11 -0400

    Start of project. Created 4 forms. SelectForm, OrderForm, StreamForm and SplashForm.

--- COMP1004-F2016-Assignment3/SplashForm.Designer.cs
+++ COMP1004-F2016-Assignment3/SplashForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP1004_F2016_Assignment3
 {
-    partial class Form1
+    partial class SplashForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.SplashFormTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SplashFormTimer
+            // 
+            this.SplashFormTimer.Enabled = true;
+            this.SplashFormTimer.Interval = 3000;
+            this.SplashFormTimer.Tick += new System.EventHandler(this.SplashFormTimer_Tick);
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1128, 693);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer SplashFormTimer;
     }
 }
-
---
+++ COMP1004-F2016-Assignment3/SplashForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * Application Name: Movie Bonanza
+ * Author Name: Bradley Girard
+ * Application Creation Date: Oct 24th, 2016
+ * Application Description: An application to help customers order, and stream movies
+ *                          right to their desktop.
+ */
+
+namespace COMP1004_F2016_Assignment3
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
